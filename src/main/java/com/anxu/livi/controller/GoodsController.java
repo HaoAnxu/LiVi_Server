17963b2aa61048a3224c92ed9e55a290f5cd73df
郝安旭 <haoanxu@neusoft.com>
2025-12-16 16:56:21 +0800

    日常更新

---
+++ src/main/java/com/anxu/livi/controller/GoodsController.java
@@ -0,0 +1,54 @@
+package com.anxu.livi.controller;
+
+import com.anxu.livi.common.annotation.OperateLog;
+import com.anxu.livi.model.vo.goods.GoodsDetailVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.anxu.livi.model.Result.PageResult;
+import com.anxu.livi.model.Result.Result;
+import com.anxu.livi.model.dto.goods.GoodsQueryDTO;
+import com.anxu.livi.service.GoodsService;
+/**
+ * 商品相关接口
+ *
+ * @Author: haoanxu
+ * @Date: 2025/11/7 13:32
+ */
+@Slf4j
+@RestController
+public class GoodsController {
+    @Autowired
+    private GoodsService goodsService;
+
+    //    查询商品列表
+    @OperateLog("查询商品列表")
+    @PostMapping("/goods/queryGoods")
+    public Result queryGoods(@RequestBody GoodsQueryDTO goodsQueryDto){
+        log.info("查询商品信息，参数：{}", goodsQueryDto);
+        PageResult pageResult = goodsService.queryGoods(goodsQueryDto);
+        return Result.success(pageResult);
+    }
+
+    //    更新商品评分和评论数量
+    @OperateLog("更新商品评分和评论数量")
+    @GetMapping("/goods/resetScore")
+    public Result resetScore(){
+        log.info("更新商品评分和评论数量");
+        Integer updateProductCount = goodsService.resetScore();
+        if(updateProductCount > 0) {
+            return Result.success(updateProductCount);
+        } else {
+            return Result.error("更新商品评分和评论数量失败");
+        }
+    }
+
+    //    查询单个商品详情
+    @OperateLog("查询单个商品详情")
+    @PostMapping("/goods/queryGoodsDetail")
+    public Result queryGoodsDetail(@RequestParam Long goodsId){
+        log.info("查询商品详情，参数：{}",goodsId);
+        GoodsDetailVO goodsDetailVO = goodsService.queryGoodsDetail(goodsId);
+        return Result.success(goodsDetailVO);
+    }
+}
---
+++ src/main/java/com/anxu/livi/service/GoodsService.java
@@ -0,0 +1,20 @@
+package com.anxu.livi.service;
+
+import com.anxu.livi.model.Result.PageResult;
+import com.anxu.livi.model.vo.goods.GoodsDetailVO;
+import com.anxu.livi.model.dto.goods.GoodsQueryDTO;
+
+/**
+ * 商品相关服务接口
+ *
+ * @Author: haoanxu
+ * @Date: 2025/11/7 13:32
+ */
+public interface GoodsService {
+    //    查询商品列表
+    PageResult queryGoods(GoodsQueryDTO goodsQueryDto);
+    //    更新并重新统计商品评分
+    Integer resetScore();
+    //    查询单个商品详情
+    GoodsDetailVO queryGoodsDetail(Long goodsId);
+}
